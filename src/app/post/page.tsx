40c67c7b7fd7660b0854a956ee3c192401c09652
Wiktor <80409661+WitroxHunter@users.noreply.github.com>
2025-05-31 20:42:44 +0200

    Eslint fixes 3

---
+++ src/app/post/page.tsx
@@ -0,0 +1,11 @@
+// src/app/post/page.tsx
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Post Page</h1>
+      <p>To jest strona posta.</p>
+    </div>
+  );
+}
