5cb31c62cb10cb9f021fb4b18647cc5a709b44a7
Wiktor <80409661+WitroxHunter@users.noreply.github.com>
2025-05-31 20:52:41 +0200

    Update route.ts

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -26,6 +26,7 @@ export async function GET() {
     }
 
     return NextResponse.json({ user });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (err) {
     return NextResponse.json({ user: null }, { status: 401 });
   }
