7e56f0c59cd1edaa89f460d26f6dfd0723c6d1fa
Wiktor <80409661+WitroxHunter@users.noreply.github.com>
2025-08-14 01:39:13 +0200

    Update page.tsx

--- src/app/profile/[name]/page.tsx
+++ src/app/profile/[name]/page.tsx
@@ -3,11 +3,11 @@ import { notFound } from "next/navigation";
 import { getUserFromCookie } from "@/lib/getUserFromCookie";
 import UserProfile from "@/components/UserProfile";
 
-interface ProfilePageProps {
+export default async function ProfilePage({
+  params,
+}: {
   params: { name: string };
-}
-
-export default async function ProfilePage({ params }: ProfilePageProps) {
+}) {
   const loggedInUser = await getUserFromCookie();
 
   const profileUser = await prisma.user.findUnique({
