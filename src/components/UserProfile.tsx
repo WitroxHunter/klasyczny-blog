aa5f49476e01a4559307d7f4120fc449e7eabd23
Wiktor <80409661+WitroxHunter@users.noreply.github.com>
2025-08-14 01:27:49 +0200

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { useForm } from "react-hook-form";
 import PostList from "./Posts-list";
 
